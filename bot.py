aead99a37689d86f92054519667087792dbb1bde
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-22 21:23:27 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import sys
+import tweepy
+import datetime
+
+from local_settings import *  
+
+auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
+auth.set_access_token(Access_token, Access_token_secret)
+api = tweepy.API(auth)
+
+if __name__ == '__main__':
+    print("about to update status...")
+    api.update_status('Right now it is ' + str(datetime.datetime.now()))
